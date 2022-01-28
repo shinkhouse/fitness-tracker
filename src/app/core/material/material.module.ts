75f2fe6c2e4c1a547ab0d9d207c3f32dfe36a305
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-28 14:03:33 -0600

    add input, select, form field modules

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
-  exports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule, MatInputModule, MatSelectModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatToolbarModule, MatInputModule, MatSelectModule, MatFormFieldModule],
 })
 export class MaterialModule {}
