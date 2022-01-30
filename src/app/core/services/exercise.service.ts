c7e47cc48fd337a54720cb6b2e4643ce2df0dd8d
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-30 16:33:54 -0600

    create exercise service

---
+++ src/app/core/services/exercise.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Exercise } from '../models/exercise.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ExerciseService {
+    exerciseChanged: Subject<Exercise> = new Subject<Exercise>();
+    private availableExercises: Exercise[] = [
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+        { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
+    ];
+    private runningExercise!: Exercise;
+    private exercises: Exercise[] = [];
+    constructor() {}
+
+    getAvailableExercises(): Exercise[] {
+        return [...this.availableExercises];
+    }
+
+    getPastExercises(): Exercise[] {
+      return [...this.exercises];
+    }
+
+    startExercise(selectedId: string) {
+        const selectedExercise = this.availableExercises.find((exercise) => {
+            return exercise.id === selectedId;
+        });
+
+        this.runningExercise = selectedExercise;
+        this.exerciseChanged.next({ ...this.runningExercise });
+    }
+
+    completeExercise() {
+        this.exercises.push({ ...this.runningExercise, date: new Date(), state: 'completed' });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    cancelExercise(progress: number) {
+        this.exercises.push({
+            ...this.runningExercise,
+            duration: this.runningExercise.duration * (progress / 100),
+            calories: this.runningExercise.calories * (progress / 100),
+            date: new Date(),
+            state: 'cancelled',
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    getRunningExercise(): Exercise {
+        return { ...this.runningExercise };
+    }
+}
