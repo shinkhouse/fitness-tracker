d4968987b020abcd96eda6f5fc1d66b00c9c2e93
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-30 21:11:42 -0600

    get items from firebase

--- src/app/pages/training/pages/new-training/new-training.component.ts
+++ src/app/pages/training/pages/new-training/new-training.component.ts
@@ -2,23 +2,29 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Exercise } from 'src/app/core/models/exercise.model';
 import { ExerciseService } from 'src/app/core/services/exercise.service';
-
+import { Observable } from 'rxjs';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 @Component({
     selector: 'app-new-training',
     templateUrl: './new-training.component.html',
     styleUrls: ['./new-training.component.scss'],
 })
 export class NewTrainingComponent implements OnInit {
-    public exercise: Exercise
-    constructor(private exerciseService: ExerciseService) {}
-
-    ngOnInit(): void {}
+    public exercise: Exercise;
+    public collectionRef: any;
+    availableExercises$: Observable<Exercise[]>;
+    constructor(private exerciseService: ExerciseService, private firestore: Firestore) {}
+    
+    ngOnInit(): void {
+      this.collectionRef = collection(this.firestore, 'availableExercises');
+        this.availableExercises$ = collectionData(this.collectionRef);
+    }
 
     onStartTraining(form: NgForm) {
-      this.exerciseService.startExercise(form.value.exercise);
+        this.exerciseService.startExercise(form.value.exercise);
     }
 
     getAvailableExercises(): Exercise[] {
-      return this.exerciseService.getAvailableExercises();
+        return this.exerciseService.getAvailableExercises();
     }
 }
--- src/app/pages/training/pages/new-training/new-training.module.ts
+++ src/app/pages/training/pages/new-training/new-training.module.ts
@@ -18,7 +18,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [
     NewTrainingComponent
