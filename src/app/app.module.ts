c8aa9acc4320597efe979b5a1689af7f1f0ce66e
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-30 20:49:31 -0600

    add angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { SidenavigationModule } from './core/components/sidenavigation/sidenavig
 import { StopTrainingModule } from './pages/training/pages/stop-training/stop-training.module';
 import { AuthService } from './core/services/auth.service';
 import { ExerciseService } from './core/services/exercise.service';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -27,7 +31,10 @@ import { ExerciseService } from './core/services/exercise.service';
     ReactiveFormsModule,
     HeaderModule,
     SidenavigationModule,
-    StopTrainingModule
+    StopTrainingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [AuthService, ExerciseService],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'shinkh-fitness-tracker',
+    appId: '1:154883425519:web:d4baad8f53ea4f8794471a',
+    storageBucket: 'shinkh-fitness-tracker.appspot.com',
+    apiKey: 'AIzaSyAdTGjDsS3P1LDk_0yPrbXdZ4jaCcQAIu4',
+    authDomain: 'shinkh-fitness-tracker.firebaseapp.com',
+    messagingSenderId: '154883425519',
+  },
   production: true
 };
