b6c21b74338c1c3411e1c6c2eb2ca416fe29bea8
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-31 14:52:29 -0600

    only take one from get authenticated store

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { AuthService } from '../services/auth/auth.service';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../../app.reducer';
@@ -11,7 +11,7 @@ import * as fromRoot from '../../app.reducer';
 export class AuthGuard implements CanActivate {
     constructor(private store: Store<fromRoot.State>, private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        if (this.store.select(fromRoot.getIsAuthenticated)) {
+        if (this.store.select(fromRoot.getIsAuthenticated).pipe(take(1))) {
             return true;
         } else {
             this.router.navigate(['/login']);
